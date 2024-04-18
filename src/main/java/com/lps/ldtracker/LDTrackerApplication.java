d6a9e71f6a95e0b0dbcb9b71f753e868188e06a1
MarkLL <markjoseph.llaneta@pccw.com>
2024-04-18 16:34:56 +0800

    Refactored backend artifacts, added H2 in memory database

--- src/main/java/com/lps/ldtracker/LDTrackerApplication.java
+++ src/main/java/com/lps/ldtracker/LDTrackerApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"com.lps.hris"})
+@ComponentScan(basePackages = {"com.lps.ldtracker"})
 public class LDTrackerApplication {
 
 	public static void main(String[] args) {
