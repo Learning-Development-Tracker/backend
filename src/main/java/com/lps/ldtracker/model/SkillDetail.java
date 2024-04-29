a086efa83c4ffded34dc4857aae2b56d5ecb74d1
Mike <michaelalfonse.r.lobendino@pccw.com>
2024-04-29 09:44:11 +0800

    Admin getters and setters update

--- src/main/java/com/lps/ldtracker/model/SkillDetail.java
+++ src/main/java/com/lps/ldtracker/model/SkillDetail.java
@@ -1,35 +1,69 @@
 package com.lps.ldtracker.model;
 
-import org.hibernate.annotations.NaturalId;
-
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.SequenceGenerator;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity 
-@Table
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-public class SkillDetail {
-	@Id
-	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "skilldtl")
-	@SequenceGenerator(sequenceName = "skilldtl_seq", allocationSize = 1, name = "skilldtl")
-	private String skillDetailId;
-	@NaturalId(mutable=true)
+public class SkillDetail extends SkillSet{
+	
+	private String skillId;
+	private String isActive;
 	private String skillName;
-	private String description;
-	private boolean isActive=false;
+	private String skillDesc;
+	private String isDeleted;
 	private String createdBy;
 	private String createdDate;
 	private String updatedBy;
 	private String updatedDate;
+	
+	public String getSkillId() {
+		return skillId;
+	}
+	public void setSkillId(String skillId) {
+		this.skillId = skillId;
+	}
+	public String getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(String isActive) {
+		this.isActive = isActive;
+	}
+	public String getSkillName() {
+		return skillName;
+	}
+	public void setSkillName(String skillName) {
+		this.skillName = skillName;
+	}
+	public String getSkillDesc() {
+		return skillDesc;
+	}
+	public void setSkillDesc(String skillDesc) {
+		this.skillDesc = skillDesc;
+	}
+	public String getIsDeleted() {
+		return isDeleted;
+	}
+	public void setIsDeleted(String isDeleted) {
+		this.isDeleted = isDeleted;
+	}
+	public String getCreatedBy() {
+		return createdBy;
+	}
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	public String getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+	public String getUpdatedBy() {
+		return updatedBy;
+	}
+	public void setUpdatedBy(String updatedBy) {
+		this.updatedBy = updatedBy;
+	}
+	public String getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(String updatedDate) {
+		this.updatedDate = updatedDate;
+	}
 }
