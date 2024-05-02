c0d720b21c1229ec075ac0d805f34d2ff4d1f0be
Mike <michaelalfonse.r.lobendino@pccw.com>
2024-05-02 11:24:15 +0800

    Admin api update

--- src/main/java/com/lps/ldtracker/controller/AdminController.java
+++ src/main/java/com/lps/ldtracker/controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -25,17 +26,24 @@ import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/v1/admin")
-@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:4200", maxAge = 3600)
 public class AdminController {
 	
 	@Autowired
 	private AdminService adm;
 	
-	@GetMapping(value="/getMemberSkillSet")
-	public ResponseEntity<Result> getMemberSkillSet(@RequestBody SkillDetail request) {
+	@PostMapping(value="/getMemberSkillSet")
+	public ResponseEntity<Result> getMemberSkillSet(@RequestBody String strMemberID) {
 		Result result = new Result();
-		List<SkillDetail> skillDtl = adm.getMemberSkillSet(request.getMemberId());
-		result.setData(skillDtl);
+		try {
+			List<SkillDetail> skillDtl = adm.getMemberSkillSet(strMemberID);
+			result.setData(skillDtl);
+			result.setStatus("SUCCESS");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			result.setStatus("FAILED");
+			e.printStackTrace();
+		}
 		return new ResponseEntity<>(result, HttpStatus.OK);
 	}
 
