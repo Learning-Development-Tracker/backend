b384a18f41ca5b1e331e454be7877beaa09e2dc2
bryan_borja <borjabryanjay@gmail.com>
2024-05-03 15:34:15 +0800

    registration backend v1

---
+++ src/main/java/com/lps/ldtracker/repository/UserDtlRepository.java
@@ -0,0 +1,13 @@
+package com.lps.ldtracker.repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lps.ldtracker.model.UserDtl;
+
+
+public interface UserDtlRepository extends JpaRepository<UserDtl, String> {
+	Optional<UserDtl> findByUserName(String username);
+}
