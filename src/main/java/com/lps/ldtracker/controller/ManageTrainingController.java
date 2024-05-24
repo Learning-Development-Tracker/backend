1f170000776f48e13fac54713e3a6f709c936422
mnaveraExt <mnavera.ext@gmail.com>
2024-05-24 10:14:06 +0800

    admin view calendar screen initial commit

--- src/main/java/com/lps/ldtracker/controller/ManageTrainingController.java
+++ src/main/java/com/lps/ldtracker/controller/ManageTrainingController.java
@@ -20,6 +20,7 @@ import com.lps.ldtracker.service.ManageTrainingService;
 
 import com.lps.ldtracker.model.LdTrackerError;
 import com.lps.ldtracker.service.ResultService;
+import com.lps.ldtracker.service.ViewCalendarScheduleService;
 import com.lps.ldtracker.serviceImpl.ErrorHandlingService;
 
 import lombok.RequiredArgsConstructor;
@@ -30,6 +31,7 @@ import java.util.Optional;
 
 import com.lps.ldtracker.model.Training_Dtl;
 import com.lps.ldtracker.repository.TrainingRepository;
+import com.lps.ldtracker.dto.ViewCalenderScheduleDto;
 
 
 @RestController
@@ -45,6 +47,9 @@ public class ManageTrainingController {
 	
 	@Autowired
 	ManageTrainingService manageTrainingService;
+	
+	@Autowired
+	ViewCalendarScheduleService viewCalendarScheduleService;
 	
 	
 	@GetMapping(value="/getTrainingList")
@@ -109,5 +114,19 @@ public class ManageTrainingController {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
         }
     }
+    
+    @GetMapping(value="/getViewCalendarSchedule")
+	public ResponseEntity<Result> getViewCalendarSchedule() {
+		Result result = new Result();
+		try {
+			List<ViewCalenderScheduleDto> calendarSched =  this.viewCalendarScheduleService.getViewCalendarSchedule();
+			result.setData(calendarSched);
+			result.setStatus("SUCCESS");
+		} catch (Exception e) {
+			result.setStatus("FAILED");
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(result, HttpStatus.OK);
+	}
 	
 }
