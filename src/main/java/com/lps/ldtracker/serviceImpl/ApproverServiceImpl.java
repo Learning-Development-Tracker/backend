07e1e2b40b81c78354be887b1c24eecb3031d21f
Alviz_Aaron_Jay <aaronjay.a.alviz@pccw.com>
2024-05-29 20:24:13 +0800

    approver cert-tracking-full-details be

--- src/main/java/com/lps/ldtracker/serviceImpl/ApproverServiceImpl.java
+++ src/main/java/com/lps/ldtracker/serviceImpl/ApproverServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.sql.Date;
-import java.sql.SQLException;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
